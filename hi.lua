3233640515661f46995c5fe93a805e8e30bf3325
LuaVMX <88602791+LuaVMX@users.noreply.github.com>
2024-08-30 16:54:27 +1000

    Create hi.lua

---
+++ hi.lua
@@ -0,0 +1,28 @@
+local function GetVersionFromBuildID(ID)
+	ID = tostring(ID)
+
+	local IDSplit = ID:split("")
+	
+	local VersionString = ""
+
+	if #IDSplit > 3 then
+		for i = 1, #IDSplit - 3, 1 do
+			VersionString = VersionString..IDSplit[1]
+			table.remove(IDSplit, 1)
+		end
+	elseif #IDSplit < 3 then
+		for i = 1, 3 - #IDSplit, 1 do
+			VersionString = VersionString.."0."
+		end
+	end
+
+	for i = 1, #IDSplit, 1 do
+		VersionString = VersionString..IDSplit[i]
+		
+		if i ~= #IDSplit then
+			VersionString = VersionString.."."
+		end
+	end
+	
+	return VersionString
+end
